b6c1cbdf5834d9a14ab2551b7c6ee89e46e77416
sonam331 <70954612+sonam331@users.noreply.github.com>
2020-11-19 17:49:42 +0530

    Add files via upload

---
+++ AngryBirdsStage5-master/Pig.js
@@ -0,0 +1,21 @@
+class Pig extends BaseClass {
+  constructor(x, y){
+    super(x,y,50,50);
+    this.image = loadImage("sprites/enemy.png");
+  }
+
+display(){
+  console.log(this.body.speed);
+if(this.body.speed < 3){
+super.display();
+}
+else{
+World.remove(world,this.body);
+Push();
+this.Visiblity=this.Visiblity -5;
+Tint(255,this.Visiblity);
+image(this.image,this.body.position.x, this.body.position.y,50,50);
+Pop();
+}
+}
+};
